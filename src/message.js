0ea5f776b43687f38e3fd559e5e5911241f7a3ed
LaviIonas <laviionas772@gmail.com>
2019-03-16 16:33:01 +0000

    template data for messages

---
+++ src/message.js
@@ -0,0 +1,15 @@
+export default {
+  currentUser: {name: "Avdotya"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  msg: [
+    {
+      id: "rand1",
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      id: "rand2",
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}

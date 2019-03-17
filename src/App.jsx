047ca687a1a9ef09525b335c38770dfb42520e41
LaviIonas <laviionas772@gmail.com>
2019-03-17 17:00:49 +0000

    implemented a method to recieve and interpret info from the websocket

--- src/App.jsx
+++ src/App.jsx
@@ -11,64 +11,63 @@ class App extends Component {
 
     this.state = {
       currentUser: message.currentUser.name,
+      usersOnline: 1,
       messages: message
     }
   }
 
 //--
   componentDidMount() {
-    console.log("componentDidMount in App");
-
+    function getOnline (online) {
+      this.setState({usersOnline: online});
+    }
     this.socket.onmessage = (event) =>{
       counter++;
-      let incomingMsg = JSON.parse(event.data)
-      const newMessage = {id: counter, username: incomingMsg.username, content: incomingMsg.message}
-      this.setState ({
-        currentUser: incomingMsg.username,
-        messages: {
-          msg: [...this.state.messages.msg, newMessage]
-        }
-      })
+      let incomingMsg = JSON.parse(event.data);
+      getOnline(incomingMsg.usersOnline);
+      if(incomingMsg.aMsg){
+        const newMessage = {id: counter, username: this.state.currentUser, content: incomingMsg.message}
+        this.setState ({
+          messages: {
+            msg: [...this.state.messages.msg, newMessage]
+          }
+        })
+      } else if (incomingMsg.nameChange){
+        const newName = {id: counter, username: incomingMsg.name, content: `-${this.state.currentUser}- changed their name to -${incomingMsg.name}-`}
+        this.setState ({
+          currentUser: incomingMsg.name,
+          messages: {
+            msg: [...this.state.messages.msg, newName]
+          }
+        });
+      }
     }
-
-    setTimeout(() => {
-      console.log("Simulating msg");
-      const newMessage = {id: "rand3", username: "Maya", content: "I have your marbles!"};
-      this.setState({
-        messages: {
-          msg: [...this.state.messages.msg, newMessage]
-        }
-      })
-    }, 3000)
   }
 
   handleInputName = (name) => {
-      // console.log("Name from App.js",name);
-      // console.log("IM SENDING NOW")
-      // this.socket.send(name);
-      // this.setState({
-      //   currentUser: name
-      // })
-      /*
-      // Send text to all users through the server
-
-      */
-    }
-  handleInputMsg = (msg, name) => {
-    var message = {
-      message: msg,
-      username: name,
+    let message = {
+      name: name,
+      aMsg: false,
+      nameChange: true
     }
     this.socket.send(JSON.stringify(message));
   }
-
-
+  handleInputMsg = (msg) => {
+    let message = {
+      message: msg,
+      aMsg: true,
+      nameChange: false
+    }
+    // console.log("message about to be sent to WebSocket", message);
+    this.socket.send(JSON.stringify(message));
+  }
   render() {
     console.log("in app", this.state)
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
+          <span>{this.state.usersOnline}</span>
         </nav>
         <MessageList messages = {this.state.messages}/>
         <Chatbar currentUser = {this.state.currentUser}

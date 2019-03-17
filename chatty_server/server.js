aa5aed0831d9d7b288a6797ad3141ecef8b62201
LaviIonas <laviionas772@gmail.com>
2019-03-17 17:00:07 +0000

    updated the server to incorporate websocket

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,70 +1,22 @@
-// server.js
 const express = require('express');
-const SocketServer = require('ws').Server;
-// Set the port to 3001
+const SocketServer = require('ws');
 const PORT = 3001;
-// Create a new express server
-const server = express()
-    // Make the express server serve static assets (html, javascript, css) from the /public folder
-    .use(express.static('public')).listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
-// Create the WebSockets server
-const wss = new SocketServer({
-    server
-});
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
+const server = express().use(express.static('public')).listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+const wss = new SocketServer.Server({server});
+wss.on('connection', function connection(ws) {
+    // console.log(wss.clients.size);
+
+    ws.on('message', function incoming(data) {
+        // Broadcast to everyone else.
+        let incomingData = JSON.parse(data);
+        incomingData.usersOnline = wss.clients.size;
+        console.log(incomingData);
+        //json parse data, add key, strigify and send
+        wss.clients.forEach(function each(client) {
             if (client.readyState === SocketServer.OPEN) {
-                client.send(data);
+                client.send(JSON.stringify(incomingData));
             }
         });
-};
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    // console.log("ws:", ws);
-    ws.on("message", (event) => {
-        ws.send(event);
-
     });
-    // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-    ws.on('close', () => console.log('Client disconnected'));
 });
-/*
-  exampleSocket.onmessage = function(event) {
-  var f = document.getElementById("chatbox").contentDocument;
-  var text = "";
-  var msg = JSON.parse(event.data);
-  var time = new Date(msg.date);
-  var timeStr = time.toLocaleTimeString();
 
-  switch(msg.type) {
-    case "id":
-      clientID = msg.id;
-      setUsername();
-      break;
-    case "username":
-      text = "<b>User <em>" + msg.name + "</em> signed in at " + timeStr + "</b><br>";
-      break;
-    case "message":
-      text = "(" + timeStr + ") <b>" + msg.name + "</b>: " + msg.text + "<br>";
-      break;
-    case "rejectusername":
-      text = "<b>Your username has been set to <em>" + msg.name + "</em> because the name you chose is in use.</b><br>"
-      break;
-    case "userlist":
-      var ul = "";
-      for (i=0; i < msg.users.length; i++) {
-        ul += msg.users[i] + "<br>";
-      }
-      document.getElementById("userlistbox").innerHTML = ul;
-      break;
-  }
-
-  if (text.length) {
-    f.write(text);
-    document.getElementById("chatbox").contentWindow.scrollByPages(1);
-  }
-};
-*/

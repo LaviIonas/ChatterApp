a938a1bd75d2b0179e79218790edcc7979651a77
LaviIonas <laviionas772@gmail.com>
2019-03-25 14:29:46 +0000

    fixed up the websocket to count online users and send out messages as broadcasts

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,21 +1,67 @@
+//server.js
 const express = require('express');
-const SocketServer = require('ws');
+const uuidV4 = require('uuid/v4');
+//
+const SocketServer = require('ws').Server;
 const PORT = 3001;
-const server = express().use(express.static('public')).listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
-const wss = new SocketServer.Server({server});
+//
+const server = express()
+    .use(express.static('public'))
+    .listen(PORT, '0.0.0.0', 'localhost', () =>
+        console.log(`Listening on ${ PORT }`));
+//
+const wss = new SocketServer({ server });
+//
+function sendMessage(socket, messageAsString) {
+  try {
+    socket.send(messageAsString, (error) => {
+      if(error) {
+        console.log(error);
+      }
+    });
+  } catch(error){
+    console.log(error);
+  }
+}
+//
+function broadcastMessage(message) {
+  const messageAsString = JSON.stringify(message);
+  wss.clients.forEach(function each(client) {
+    sendMessage(client, messageAsString);
+  });
+}
+//
+function systemMessage(event, onlineCount) {
+  const message = {
+    id: uuidV4(),
+    type: "Status",
+    event: event,
+    onlineCount: onlineCount
+  };
+  return message;
+}
+//
 wss.on('connection', function connection(ws) {
-    // console.log(wss.clients.size);
+    broadcastMessage(systemMessage("connected", wss.clients.size));
 
     ws.on('message', function incoming(data) {
-        // Broadcast to everyone else.
+        console.log("recieved message");
         let incomingData = JSON.parse(data);
-        incomingData.usersOnline = wss.clients.size;
-        //json parse data, add key, strigify and send
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === SocketServer.OPEN) {
-                client.send(JSON.stringify(incomingData));
-            }
-        });
+        incomingData.id = uuidV4();
+
+        // Broadcast to all.
+        broadcastMessage(incomingData);
+    });
+
+    ws.on('close', () =>{
+        const disconnectMessage = JSON.stringify(systemMessage("disconnected", wss.clients.length));
+
+        // send an updated to everyone else except for the disconnected users
+        wss.clients
+            .filter(clientSocket => clientSocket !== ws)
+            .forEach(function each(client) {
+              safeSend(client, disconnectMessage);
+            });
+        console.log('Client disconnected', wss.clients.length);
     });
 });
-

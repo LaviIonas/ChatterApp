aeac93b5cab300407f570a4c36791f9f5b29ddd4
LaviIonas <laviionas772@gmail.com>
2019-04-08 19:22:24 +0000

    added comments and cleaned up the format

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,7 +1,6 @@
 //server.js
 const express = require('express');
 const uuidV4 = require('uuid/v4');
-//
 const SocketServer = require('ws').Server;
 const PORT = 3001;
 //
@@ -9,9 +8,8 @@ const server = express()
     .use(express.static('public'))
     .listen(PORT, '0.0.0.0', 'localhost', () =>
         console.log(`Listening on ${ PORT }`));
-//
 const wss = new SocketServer({ server });
-//
+//function to send message with error handling
 function sendMessage(socket, messageAsString) {
   try {
     socket.send(messageAsString, (error) => {
@@ -23,14 +21,14 @@ function sendMessage(socket, messageAsString) {
     console.log(error);
   }
 }
-//
+//function to send message to each existing client (using a forEach)
 function broadcastMessage(message) {
   const messageAsString = JSON.stringify(message);
   wss.clients.forEach(function each(client) {
     sendMessage(client, messageAsString);
   });
 }
-//
+//function to construct a 'system' message that notifies the users console of online activity (connections and disconections)
 function systemMessage(event, onlineCount) {
   const message = {
     id: uuidV4(),
@@ -41,11 +39,13 @@ function systemMessage(event, onlineCount) {
   console.log(message);
   return message;
 }
-//
+//Upon connection, run following code
 wss.on('connection', function connection(ws) {
     console.log("Client connected; number of clients connected:", wss.clients.size);
+    //broadcasts to all clients a system message that a new user connected with the amount of current users
     broadcastMessage(systemMessage("connected", wss.clients.size));
 
+    //On message parse incoming data and assign a uuidV4 to each message
     ws.on('message', function incoming(data) {
         console.log("recieved message");
         let incomingData = JSON.parse(data);
@@ -55,10 +55,10 @@ wss.on('connection', function connection(ws) {
         broadcastMessage(incomingData);
     });
 
+    //On close once again broadcast to clients' console that a user has disconnected
     ws.on('close', () =>{
         const disconnectMessage = JSON.stringify(systemMessage("disconnected", wss.clients.size));
-
-        // send an updated to everyone else except for the disconnected users
+        // send an updated to everyone
         wss.clients.forEach(function each(client) {
               sendMessage(client, disconnectMessage);
             });
